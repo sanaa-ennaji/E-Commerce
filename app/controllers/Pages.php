502f34f87bc1eac9e241b8b899124e82ec065406
SpangorBen <netflixbenzzinbi@gmail.com>
2024-01-03 15:22:10 +0100

    Client Service

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -5,6 +5,14 @@ class Pages extends Controller
     public function index()
     {
     }
+
+    public function navbar(){
+        $this->view('pages/navbar');
+    }
+
+    public function home(){
+        $this->view('pages/home');
+    }
 }
 ?>
 
