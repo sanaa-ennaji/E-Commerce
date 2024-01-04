bea7678b6193e7e92785e0e229107045b02c7272
SpangorBen <netflixbenzzinbi@gmail.com>
2024-01-04 12:21:57 +0100

    fix Client

--- app/controllers/Client.php
+++ app/controllers/Client.php
@@ -5,6 +5,10 @@ class Client extends Controller {
     public function shop() {
         $this->view("client/shop");
     }
+    public function cart(){
+        $this->view('client/panier');
+    }
+    
 }
 
 ?>
