31ee292c63d3383fd966d083937fd9b13d67c870
SpangorBen <netflixbenzzinbi@gmail.com>
2024-01-04 14:03:14 +0100

    fixing

--- app/controllers/Client.php
+++ app/controllers/Client.php
@@ -11,17 +11,4 @@ class Client extends Controller {
    
 }
 
-?><?php 
-
-class Client extends Controller {
-
-    public function shop() {
-        $this->view("client/shop");
-    }
-    public function cart(){
-        $this->view('client/panier');
-    }
-    
-}
-
 ?>
--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -6,10 +6,6 @@ class Pages extends Controller
     {
 
     }
-<<<<<<< HEAD
-=======
-
->>>>>>> 23ae17a030aca09f269aea10253f991698e1ffd2
 
 
     public function navbar(){
@@ -19,16 +15,6 @@ class Pages extends Controller
     public function home(){
         $this->view('pages/home');
     }
-<<<<<<< HEAD
- 
-
-=======
-
-    public function cart(){
-        $this->view('client/cart');
-    }
-   
->>>>>>> 23ae17a030aca09f269aea10253f991698e1ffd2
 
     public function cart(){
         $this->view('client/cart');
