b3dbe2a0652d4e71c1d00fec93c3276226f3393b
sanaa <sanaaennaji93@gmail.com>
2024-01-04 15:12:08 +0100

    add commande line service

--- app/views/pages/navbar.php
+++ app/views/pages/navbar.php
@@ -1,9 +1,5 @@
 
 <?php require APPROOT. '/views/incfiles/header.php'?>
-<<<<<<< HEAD
-=======
-
->>>>>>> 5187b38850101d2a3d26a651b890850043663971
 <nav class="relative bg-white shadow">
     <div class="container px-6 py-4 mx-auto md:flex md:justify-between md:items-center">
         <div class="flex items-center justify-between">
