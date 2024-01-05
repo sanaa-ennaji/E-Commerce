451872547aedc0d7210a625e41cd4ddf58315093
sanaa ennaji <144794786+sanaa-ennaji@users.noreply.github.com>
2024-01-05 09:57:13 +0100

    Update interfaceCategory.php

--- app/services/interfaceCategory.php
+++ app/services/interfaceCategory.php
@@ -3,11 +3,10 @@
     interface interfaceCategory {
         public function addCategory(Category $category);
         public function getAllCategories();
-
         public function searchCategory($r);
         public function deleteCategory($id);
         public function updateCategory(Category $category);
+        
 
     }
 ?>
-
