0f9bfe968636f4a2d1e5bea3d075d66efe5bd7ef
Mohamed Aymen Azza <mohamedaymenazza@gmail.com>
2024-01-05 11:18:29 +0100

    Committing changes before merge

---
+++ app/views/pages/home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Document</title>
+</head>
+<body>
+    <section>
+        <div class="bg-cover bg-center h-screen" style="background-image: url('app/views/images/banner-bg.jpg.webp');">
+            
+        </div>
+    </section>
+</body>
+</html> 
